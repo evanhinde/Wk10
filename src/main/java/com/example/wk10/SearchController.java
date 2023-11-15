336eb64376d57f6a80e3a142f53c9c2b3b860986
evanhinde <200529232@student.georgianc.on.ca>
2023-11-14 19:39:04 -0500

    wk11 update scene

--- src/main/java/com/example/wk10/SearchController.java
+++ src/main/java/com/example/wk10/SearchController.java
@@ -3,6 +3,8 @@ package com.example.wk10;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -22,10 +24,19 @@ public class SearchController  implements Initializable {
     @FXML
     private TextField searchTextField;
 
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Button getDetailsButton;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        errorLabel.setVisible(false);
+        getDetailsButton.setVisible(false);
         listView.getSelectionModel().selectedItemProperty().addListener(
                 ((observableValue, oldMovie, newMovie) ->  {
+                    getDetailsButton.setVisible(true);
                     try {
                         imageView.setImage(new Image(newMovie.getPoster()));
                     } catch (Exception e) {
@@ -43,7 +54,18 @@ public class SearchController  implements Initializable {
         ApiResponse apiResponse = ApiUtility.getDataFromAPIQuick(searchString);
 
         // add movie objects to listView
-        listView.getItems().addAll(apiResponse.getMovie());
+        if (apiResponse.getMovie() == null) {
+            errorLabel.setVisible(true);
+            errorLabel.setText("No Movies Found");
+        } else {
+            listView.getItems().addAll(apiResponse.getMovie());
+            errorLabel.setVisible(false);
+        }
+    }
+
+    @FXML
+    void getDetailsButton_onClick(ActionEvent event) {
+
     }
 
 
